d9ac80e9d0000cf118c890d653c3b13cd37bcbbf
brandonsams <brandonkeithsams@gmail.com>
2022-06-12 14:20:27 -0600

    Initial Commit

---
+++ vision-test/vision_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  vision_testApp.swift
+//  vision-test
+//
+//  Created by Brandon Sams on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct vision_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
